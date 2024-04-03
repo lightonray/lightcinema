8c4f4a03fe750b825950d9c3f1d64475b95c3275
rei kasemi <reikasemi2001@gmail.com>
2024-04-04 02:12:09 +0300

    fixed background images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -991,13 +991,13 @@
       <section class="cta">
         <div class="container">
 
-          <div class="title-wrapper">
+          {{-- <div class="title-wrapper">
             <h2 class="cta-title">Trial start first 30 days.</h2>
 
             <p class="cta-text">
               Enter your email to create or restart your membership.
             </p>
-          </div>
+          </div> --}}
 
           <form action="" class="cta-form">
             <input type="email" name="email" required placeholder="Enter your email" class="email-field">
