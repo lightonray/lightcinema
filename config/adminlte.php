241f412fdbc1733c397bc78e45891b48214456a9
rei kasemi <reikasemi2001@gmail.com>
2024-04-21 22:09:22 +0300

    fixes

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>LightCinema</b>',
+    'logo' => '<b>- LightCinema</b>',
     'logo_img' => 'vendor/adminlte/dist/img/lcinemalogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
