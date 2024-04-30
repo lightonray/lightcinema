e189df088d631501d009fc70cd59b20ae89304cd
rei kasemi <reikasemi2001@gmail.com>
2024-04-30 19:43:24 +0300

    added unit tests

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    use RefreshDatabase;
+
+    /** @test */
+    public function login_page_is_accessible()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function user_can_login_with_correct_credentials()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect('/home');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function user_cannot_login_with_incorrect_password()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('correct-password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertGuest();
+    }
+}
